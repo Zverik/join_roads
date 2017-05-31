5ae988b4bb7db82930257fcec6a672b03b851c82
Ilya Zverev <zverik@textual.ru>
2017-05-31 14:52:26 +0300

    Optimize a bit

--- src/join_roads.cpp
+++ src/join_roads.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <ctime>
+#include <list>
 #include <unordered_map>
 #include <algorithm>
 #include <iostream>
@@ -13,46 +14,48 @@
 
 std::uint32_t constexpr kMaxWayId = 500000000;
 
-using TWayId = std::uint32_t;
-using TNodeId = std::uint64_t;
-using TWays = std::deque<std::pair<TWayId, bool>>;
+using TWays = std::deque<std::int32_t>;
 
 class LineString
 {
   TWays m_ways;
-  std::pair<TNodeId, TNodeId> m_ends;
+  std::uint64_t m_start;
+  std::uint64_t m_end;
 
 public:
   explicit LineString(osmium::Way const & way)
   {
-    bool reverse = way.tags().has_tag("oneway", "-1");
-    m_ways.push_back({way.id(), reverse});
-    m_ends = {way.nodes().front().ref(), way.nodes().back().ref()};
+    bool const reverse = way.tags().has_tag("oneway", "-1");
+    m_ways.push_back(reverse ? -way.id() : way.id());
+    m_start = way.nodes().front().ref();
+    m_end = way.nodes().back().ref();
   }
 
   TWays const & Ways() const { return m_ways; }
 
   void reverse()
   {
-    m_ends = {m_ends.second, m_ends.first};
+    auto const tmp = m_end;
+    m_end = m_start;
+    m_start = tmp;
     std::reverse(m_ways.begin(), m_ways.end());
     for (auto & p : m_ways)
-      p.second = !p.second;
+      p = -p;
   }
 
   bool add(LineString & line)
   {
-    if (m_ends.first == line.m_ends.first || m_ends.second == line.m_ends.second)
+    if (m_start == line.m_start || m_end == line.m_end)
       line.reverse();
-    if (m_ends.second == line.m_ends.first)
+    if (m_end == line.m_start)
     {
       m_ways.insert(m_ways.end(), line.m_ways.begin(), line.m_ways.end());
-      m_ends.second = line.m_ends.second;
+      m_end = line.m_end;
     }
-    else if (m_ends.first == line.m_ends.second)
+    else if (m_start == line.m_end)
     {
       m_ways.insert(m_ways.begin(), line.m_ways.begin(), line.m_ways.end());
-      m_ends.first = line.m_ends.first;
+      m_start = line.m_start;
     }
     else
       return false;
@@ -62,7 +65,7 @@ public:
 
 class Segments
 {
-  std::vector<LineString> m_parts;
+  std::list<LineString> m_parts;
 
 public:
   explicit Segments(osmium::Way const & way)
@@ -125,18 +128,11 @@ public:
       auto const & longWays = seg.second.getLongWays();
       for (TWays const & ways : longWays)
       {
-        if (ways.size() <= 1)
-          continue;
-        bool first = true;
-        for (auto const & wayRev : ways)
+        for (auto const & way : ways)
         {
-          if (first)
-            first = false;
-          else
+          if (way != ways.front())
             std::cout << ',';
-          if (wayRev.second)
-            std::cout << '-';
-          std::cout << wayRev.first;
+          std::cout << way;
         }
         std::cout << std::endl;
       }
@@ -148,11 +144,11 @@ public:
     if (!m_count)
       m_startTime = std::time(nullptr);
     m_count++;
-    if (m_count % 100000 == 0)
+    if (!(m_count & 0xFFFFF))
     {
-      double ways_per_sec = double(m_count) / (std::time(nullptr) - m_startTime);
-      std::uint32_t elapsed = (kMaxWayId - way.id()) / ways_per_sec;
-      std::cerr << '\r' << std::setprecision(4) << (100.0 * way.id() / kMaxWayId) << "% (est. " << (elapsed / 60) << " min)   ";
+      double const ways_per_sec = double(m_count) / (std::time(nullptr) - m_startTime);
+      std::uint32_t const elapsed = (kMaxWayId - way.id()) / ways_per_sec;
+      std::cerr << '\r' << std::setprecision(3) << (100.0 * way.id() / kMaxWayId) << "% (est. " << (elapsed / 60) << " min)   ";
     }
 
     bool foundHighway = false;
@@ -170,7 +166,7 @@ public:
     if (!foundHighway || (name.empty() && ref.empty()))
       return;
 
-    std::size_t key = std::hash<std::string>{}(name + '\0' + ref);
+    std::size_t const key = std::hash<std::string>{}(name + '\0' + ref);
     auto segment = m_data.find(key);
     if (segment == m_data.cend())
       m_data.emplace(key, Segments(way));
